805f29690faa2579990cf67ff96ab697ca9c6c4d
BreakDS <breakds@gmail.com>
2012-07-12 16:58:36 -0500

    More demonstrative sample usage.

--- sample/PatchShift.cpp
+++ sample/PatchShift.cpp
@@ -39,10 +39,34 @@ int main( int argc, char **argv )
     exit( -1 );
   }
   cv::Mat mat = cv::imread( argv[1] );
-  
+
+  // Create an image with CIEL*a*b* feature descriptors
   Image<LabCell, std::pair<int,int> > img = std::move( InitCellImage( mat ) );
+
+
+  // Set the parameter for patches
+  // in this case, will be 5 x 5 patch with a cell stride of 4
   img.SetPatchParameter( 5, 5, 4 );
 
+
+  // Get a patch at coordinate (35, 102)
+  Image<LabCell, std::pair<int,int> >::Patch patch = img.Spawn( 35, 102 );
+
+
+  // Iterate the cell components of this patch, and print them out
+  for ( int i=0; i<patch.dim(); i++ ) {
+    patch(i).Summary();
+  }
+
+  // Iterate the cell components of the patch
+  for ( int i=0; i<patch.dim(); i++ ) {
+    // Iterate the feature descriptors component of the cell
+    for ( int j=0; j<patch(i).length; j++ ) {
+      printf( "%hhu ", patch(i)(j) );
+    }
+  }
+  printf( "\n" );
+  
   
   return 0;
 }
