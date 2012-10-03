ecbb30dd8a091466b0d5b22a1df4e7195c755dcc
BreakDS <breakds@gmail.com>
2012-10-03 13:31:37 -0500

    Stiching.cpp Batching Function Enabled.

--- sample/Stitching.cpp
+++ sample/Stitching.cpp
@@ -43,87 +43,90 @@ int main( int argc, char **argv )
 
   std::vector<std::string> imgList = std::move( readlines( strf( "%s/%s", env["dataset"].c_str(),
                                                                  env["list-file"].c_str() ) ) );
-  
-  PatGraph graph( env["graph-file"] );
 
-  // Stitching with blending
-  int counts[graph.rows][graph.cols];
-  float values[graph.rows][graph.cols][3];
-  for ( int i=0; i<graph.rows; i++ ) {
-    for ( int j=0; j<graph.cols; j++ ) {
-      counts[i][j] = 0;
-      for ( int k=0; k<3; k++ ) {
-        values[i][j][3] = 0;
+
+  for ( int graphID=0; graphID<env["graphs"].size(); graphID++ ) {
+    
+    PatGraph graph( strf( "%s/%s", env["graph-folder"].c_str(), env["graphs"][graphID].c_str() ) );
+
+    // Stitching with blending
+    int counts[graph.rows][graph.cols];
+    float values[graph.rows][graph.cols][3];
+    for ( int i=0; i<graph.rows; i++ ) {
+      for ( int j=0; j<graph.cols; j++ ) {
+        counts[i][j] = 0;
+        for ( int k=0; k<3; k++ ) {
+          values[i][j][k] = 0;
+        }
       }
     }
-  }
-
-
-  vector<int> imageIDs;
-  imageIDs.resize( graph.rows * graph.cols );
-  for ( int i=0; i<graph.rows*graph.cols; i++ ) {
-    imageIDs[i] = graph(i)[0].index;
-  }
-  vector<int> sorted = std::move( sorting::index_sort( imageIDs ) );
-
-
-
-  Image<BGRCell, int, false> image;
   
-  int currentImgID = -1;
-  for ( auto& i : sorted ) {
-  // for ( int i=0; i<graph.rows*graph.cols; i++ ) {
-    int y = i / graph.cols;
-    int x = i % graph.cols;
-    if ( graph(i)[0].index != currentImgID ) {
-      currentImgID = graph(i)[0].index;
-      cv::Mat raw = cv::imread( strf( "%s/%s_L.png", env["dataset"].c_str(),
-                                      imgList[currentImgID].c_str() ) );
-      if ( raw.empty() ) {
-        Error( "cannot open image %s/%s.png", env["dataset"].c_str(),
-               imgList[currentImgID].c_str() );
-        return -1;
-      }
-      image = std::move( cvFeatGen<BGRCell, int, false>::gen( raw ) );
-      image.SetPatchParameter( env["patch-w"], env["patch-w"] );
+
+    vector<int> imageIDs;
+    imageIDs.resize( graph.rows * graph.cols );
+    for ( int i=0; i<graph.rows*graph.cols; i++ ) {
+      imageIDs[i] = graph(i)[0].index;
     }
+    vector<int> sorted = std::move( sorting::index_sort( imageIDs ) );
+
+
+
+    Image<BGRCell, int, false> image;
+  
+    int currentImgID = -1;
+    for ( auto& i : sorted ) {
+      int y = i / graph.cols;
+      int x = i % graph.cols;
+      if ( graph(i)[0].index != currentImgID ) {
+        currentImgID = graph(i)[0].index;
+        cv::Mat raw = cv::imread( strf( "%s/%s_L.png", env["dataset"].c_str(),
+                                        imgList[currentImgID].c_str() ) );
+        if ( raw.empty() ) {
+          Error( "cannot open image %s/%s.png", env["dataset"].c_str(),
+                 imgList[currentImgID].c_str() );
+          return -1;
+        }
+        image = std::move( cvFeatGen<BGRCell, int, false>::gen( raw ) );
+        image.SetPatchParameter( env["patch-w"], env["patch-w"] );
+      }
 
     
-    Image<BGRCell, int, false>::Patch candPatch= Loc2Patch( image, graph(i)[0] );
+      Image<BGRCell, int, false>::Patch candPatch= Loc2Patch( image, graph(i)[0] );
 
-    int k = 0;
-    for ( int dy=0; dy<env["patch-w"]; dy++ ) {
-      for ( int dx=0; dx<env["patch-w"]; dx++ ) {
-        if ( y + dy < graph.rows && x + dx < graph.cols ) {
-          counts[y+dy][x+dx]++;
-          for ( int ch=0; ch<3; ch++ ) {
-            values[y+dy][x+dx][ch] += candPatch[k];
-            k++;
+      int k = 0;
+      for ( int dy=0; dy<env["patch-w"]; dy++ ) {
+        for ( int dx=0; dx<env["patch-w"]; dx++ ) {
+          if ( y + dy < graph.rows && x + dx < graph.cols ) {
+            counts[y+dy][x+dx]++;
+            for ( int ch=0; ch<3; ch++ ) {
+              values[y+dy][x+dx][ch] += candPatch[k];
+              k++;
+            }
+          } else {
+            k += 3;
           }
-        } else {
-          k += 3;
+
         }
-
       }
     }
+
+
+    cv::Mat result( graph.rows, graph.cols, CV_8UC3 );
+
+    for ( int i=0; i<graph.rows; i++ ) {
+      for ( int j=0; j<graph.cols; j++ ) {
+        for ( int k=0; k<3; k++ ) {
+          result.at<cv::Vec3b>(i,j)[k] = static_cast<uchar>( values[i][j][k] / counts[i][j] );
+        }
+      }
+    }
+
+    cv::imwrite( strf( "%s/%s.png", env["graph-folder"].c_str(), env["graphs"][graphID].c_str() ), result );
+
+    Done( "%d/%d.", graphID+1, env["graphs"].size() );
   }
 
 
-  cv::Mat result( graph.rows, graph.cols, CV_8UC3 );
-
-  for ( int i=0; i<graph.rows; i++ ) {
-    for ( int j=0; j<graph.cols; j++ ) {
-      for ( int k=0; k<3; k++ ) {
-        result.at<cv::Vec3b>(i,j)[k] = static_cast<uchar>( values[i][j][k] / counts[i][j] );
-      }
-    }
-  }
-
-  cv::imwrite( "tmp.png", result );
-
-  imshow( "result", result );
-
-  while ( cv::waitKey(30) != 27 );
 
 
   
